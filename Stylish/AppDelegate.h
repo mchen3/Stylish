1fba0933e7e40e31ce8192d592febe2033302946
first commit <mike_chen7@hotmail.com>
2014-07-23 22:35:33 -0400

    Initial commit

---
+++ Stylish/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stylish
+//
+//  Created by Mike Chen on 7/23/14.
+//  Copyright (c) 2014 App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Stylish/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Stylish
+//
+//  Created by Mike Chen on 7/23/14.
+//  Copyright (c) 2014 App. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
